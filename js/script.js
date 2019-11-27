446dc7287a575ca6788f89fe380282a4347616af
Kiki Wu <kikiwu404@gmail.com>
2019-11-27 13:32:40 -0800

    Update twitter link

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-var lazyLoadInstance = new LazyLoad({
-    elements_selector: ".lazy"
+$(function () {
+    $('.lazy').Lazy({
+        // your configuration goes here
+        scrollDirection: 'vertical',
+        effect: 'fadeIn',
+        visibleOnly: true,
+        onError: function (element) {
+            console.log('error loading ' + element.data('src'));
+        }
+    });
 });
